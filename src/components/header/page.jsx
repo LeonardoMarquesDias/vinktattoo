fccdb87f448b731ac37664a06df01763e8680acd
LeonardoDias <66549962+LeonardoMarquesDias@users.noreply.github.com>
2024-06-12 16:56:32 +0100

    Header onClick={() => setMobileMenuOpen(false)}

--- src/components/header/page.jsx
+++ src/components/header/page.jsx
@@ -2,18 +2,11 @@
 
 import Link from 'next/link'
 import { useState } from 'react'
-import { Dialog, DialogPanel, DialogOverlay } from '@headlessui/react'
+import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { useRouter } from 'next/router'
 
 export default function Header({ navigation = [] }) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  const router = useRouter()
-
-  const handleNavigation = (href) => {
-    setMobileMenuOpen(false)
-    router.push(href)
-  }
 
   return (
     <header className="absolute inset-x-0 top-0 z-50" id="home">
@@ -49,10 +42,10 @@ export default function Header({ navigation = [] }) {
         </div>
       </div>
       <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)}>
-        <DialogOverlay className="fixed inset-0 bg-black bg-opacity-30" />
+        <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-950 px-6 pt-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5" onClick={() => handleNavigation('/')}>
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Vinicios Capobianco</span>
               <img
                 className="h-14 w-auto"
@@ -73,15 +66,15 @@ export default function Header({ navigation = [] }) {
           </div>
           <div className="mt-6 pl-4 flow-root">
             <div className="-my-6 divide-y divide-gold-500">
-              <div className="space-y-2 py-6">
+              <div className="space-y-2 py-6" onClick={() => setMobileMenuOpen(false)}>
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:text-gold-800 cursor-pointer"
-                    onClick={() => handleNavigation(item.href)}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:text-gold-800"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="py-6"></div>
